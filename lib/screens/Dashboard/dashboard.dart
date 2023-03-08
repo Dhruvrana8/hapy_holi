e1ba11acb405a163f6bf6ac207b50ef9ccc3c1ef
Dhruv <dhruvrana4@gmail.com>
2023-03-08 11:27:54 +0530

    fix: minor changes

--- lib/screens/Dashboard/dashboard.dart
+++ lib/screens/Dashboard/dashboard.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:hapy_holi/Constants/app_strings.dart';
 import 'package:hapy_holi/screens/Dashboard/components/holi_screen.dart';
 import 'package:hapy_holi/screens/Dashboard/components/wish_screen.dart';
-import 'package:hapy_holi/screens/Dashboard/components/woomen_day_screen.dart';
+import 'package:hapy_holi/screens/Dashboard/components/women_day_screen.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:lottie/lottie.dart';
 
